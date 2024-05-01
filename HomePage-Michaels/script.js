4f15a101715230637adfbadc0400251ef808cec3
michaelkurniawan <mchlkk98@gmail.com>
2024-05-01 21:47:20 +0700

    Masukin semua asset & mulai buat navbar

---
+++ HomePage-Michaels/script.js
@@ -0,0 +1,11 @@
+
+function openHamburger() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.visibility = 'visible';
+}
+
+function hideHamburger(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
+
