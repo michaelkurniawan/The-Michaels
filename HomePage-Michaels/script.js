6c4aa85e749ae3c28ae4d30e7df8699e8fd5b1d2
michaelkurniawan <mchlkk98@gmail.com>
2024-05-07 09:36:58 +0700

    revisi navbar sm benerin dropdown

--- HomePage-Michaels/script.js
+++ HomePage-Michaels/script.js
@@ -1,12 +1,15 @@
+let navbar = document.getElementById("navbar");
 let dropdown = document.getElementById("drop");
 let hamburger = document.getElementById("icon-hamburger");
 
 function toggleHamburger(){
     dropdown.classList.toggle("open");
+    navbar.classList.toggle("navbar-open");
 }
 
 document.addEventListener("mousedown", (event) => {
     if(!dropdown.contains(event.target) && !hamburger.contains(event.target)){
         dropdown.classList.remove("open");
+        navbar.classList.remove("navbar-open");
     }
 })
